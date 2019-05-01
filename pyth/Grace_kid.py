aa579f238a92cf8eb7c80a367312fda76a7080f6
toferrari <tferrari@SHADOW-DSTOANG2.localdomain>
2019-05-01 21:23:01 +0200

    sdsd

---
+++ pyth/Grace_kid.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.4
+import os
+file="Grace_kid.py"
+f= open(file,"w")
+os.chmod(file, 447)
+chaine="#!/usr/bin/python3.4{0}import os{0}file={2}Grace_kid.py{2}{0}f= open(file,{2}w{2}){0}os.chmod(file, 447){0}chaine={2}{3}{2}{0}f.write(chaine.format(chr(10), chr(9), chr(34), chaine)){0}f.close(){0}"
+f.write(chaine.format(chr(10), chr(9), chr(34), chaine))
+f.close()
