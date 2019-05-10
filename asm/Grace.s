44985351aa4eaee2310f52bd020def986b90c37f
Tony FERRARI <tferrari@e3r5p18.42.fr>
2019-05-10 19:18:08 +0200

    grace.s add code in strng for kid

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,22 +1,21 @@
-
-%define OUI _main
+%define NO_MAIN _main
 %define NL 10
 %define TAB 9
 
-%macro no_main
+%macro no_main 0
 	SECTION .data
 	text:
-		.string db "test"
-	file
-		.string db 'Grace_kid.s', 0
+		.string db "%%define OUI _main%1$c%%define NL 10%1$c%%define TAB 9%1$c%1$c%%macro no_main 0%1$c%2$cSECTION .data%1$c%2$ctext:%1$c%2$c%2$c.string db%3$c%s%2$c%1$c%2$cfile:%1$c%2$c%2$c.string db%3$cGrace_kid.s%3$c, 0%1$c%1$c%2$cSECTION .text%1$c%2$c%2$cglobal start%1$c%2$c%2$cglobal NO_MAIN%1$c%1$c%2$cstart:%1$c%2$c%2$ccall NO_MAIN%1$c%2$c%2$cextern _dprintf%1$c%1$c%2$c_main:",10,9,9,"push rbp",10,9,9,"mov rbp, rsp",10,9,9,"lea rdi, [rel file]",10,9,9,"mov rsi, 1537" ,0
+	file:
+		.string db "Grace_kid.s", 0
 
 	SECTION .text
 		global start
-		global OUI
+		global NO_MAIN
 
 	start:
-		call _main
-	extern _dprintf
+		call NO_MAIN
+		extern _dprintf
 
 	_main:
 		push rbp
@@ -27,10 +26,11 @@
 		mov rax, 0x2000005
 		syscall
 		mov rdi, rax
-		lea rsi, [rel text]
-		lea rdx, [10]
-		lea rcx, [9]
+		lea rsi, [rel text.string]
+		lea rdx, [NL]
+		lea rcx, [TAB]
 		lea r8, [34]
+		lea r9, [rel text.string]
 		call _dprintf
 		mov esp, ebp
 		leave
