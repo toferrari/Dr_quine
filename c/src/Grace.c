3f8d5bf5b05cc1e2c133c6f761977db347e41710
Tony FERRARI <tferrari@e3r6p18.42.fr>
2019-05-22 16:09:27 +0200

    add comment grace

--- c/src/Grace.c
+++ c/src/Grace.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>//comment
 #include <fcntl.h>
 
 #define COTE 34
 #define NAME "Grace_kid.c"
-#define AGAIN(x) int main(){char*s="#include <stdio.h>%c#include <fcntl.h>%c%c#define COTE 34%c#define NAME %cGrace_kid.c%c%c#define AGAIN(x) int main(){char*s=%c%s%c;int fd = open(NAME, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);dprintf(fd,s,10,10,10,10,COTE,COTE,10,COTE,s,COTE,10,10,10);}%c%cAGAIN(x)%c";int fd = open(NAME, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);dprintf(fd,s,10,10,10,10,COTE,COTE,10,COTE,s,COTE,10,10,10);}
+#define AGAIN(x) int main(){char*s="#include <stdio.h>//comment%c#include <fcntl.h>%c%c#define COTE 34%c#define NAME %cGrace_kid.c%c%c#define AGAIN(x) int main(){char*s=%c%s%c;int fd = open(NAME, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);dprintf(fd,s,10,10,10,10,COTE,COTE,10,COTE,s,COTE,10,10,10);}%c%cAGAIN(x)%c";int fd = open(NAME, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);dprintf(fd,s,10,10,10,10,COTE,COTE,10,COTE,s,COTE,10,10,10);}
 
 AGAIN(x)
